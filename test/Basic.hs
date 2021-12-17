6dd1c87667bcdc8a7c32a99addac77f07c505c8c
Brandon Chinn <brandonchinn178@gmail.com>
2021-12-16 20:30:10 -0800

    Add failing test for README example

---
+++ test/Basic.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Basic where
+
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import Database.Persist.Monad
+import Database.Persist.Monad.TestUtils
+import Example
+
+tests :: TestTree
+tests = testGroup "Basic functionality tests"
+  [ testCase "Ensure withTransaction README example typechecks" $ do
+      let foo :: MonadSqlQuery m => m ()
+          foo = insert_ $ person "Alice"
+          bar :: MonadSqlQuery m => m ()
+          bar = insert_ $ person "Bob"
+          fooAndBar :: MonadSqlQuery m => m ()
+          fooAndBar = withTransaction $ foo >> bar
+      runMockSqlQueryT fooAndBar
+        [ withRecord @Person $ \case
+            Insert_ _ -> Just ()
+            _ -> Nothing
+        , withRecord @Person $ \case
+            Insert_ _ -> Just ()
+            _ -> Nothing
+        ]
+  ]
