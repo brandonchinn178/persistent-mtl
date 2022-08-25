320cae6e3e626522e0d2c953dcbd694f1428c7a8
Brandon Chinn <brandonchinn178@gmail.com>
2022-08-24 22:40:06 -0700

    Move MockSqlQueryT tests into MockedTest.hs

--- test/MockedTest.hs
+++ test/MockedTest.hs
@@ -16,11 +16,13 @@ import Database.Persist.Sql (
   Entity (..),
   Single (..),
   toPersistValue,
+  toSqlKey,
   (=.),
   (==.),
  )
 import Test.Tasty
 import Test.Tasty.HUnit
+import UnliftIO (SomeException, try)
 
 import Database.Persist.Monad
 import Database.Persist.Monad.TestUtils
@@ -36,6 +38,34 @@ test =
               Insert_ _ -> Just ()
               _ -> Nothing
           ]
+    ]
+
+test =
+  testGroup
+    "MockSqlQueryT"
+    [ testCase "it errors if it could not find a mock" $ do
+        result <- try $ runMockSqlQueryT getPeopleNames []
+        case result of
+          Right _ -> assertFailure "runMockSqlQueryT did not fail"
+          Left e -> do
+            let msg = head $ lines $ show (e :: SomeException)
+            msg @?= "Could not find mock for query: SelectList{..}<Person>"
+    , testCase "it continues after a mock doesn't match" $ do
+        result <-
+          runMockSqlQueryT
+            getPeopleNames
+            [ withRecord @Post $ \_ -> error "getPeopleNames matched Post record"
+            , mockQuery $ \_ -> Nothing
+            , withRecord @Person $ \case
+                SelectList _ _ ->
+                  Just
+                    [ Entity (toSqlKey 1) (Person "Alice" 10)
+                    , Entity (toSqlKey 2) (Person "Bob" 20)
+                    ]
+                _ -> Nothing
+            ]
+
+        result @?= ["Alice", "Bob"]
     ]
 
 test =
