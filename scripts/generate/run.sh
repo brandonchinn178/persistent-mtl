6f65b69c2b0efa765b18c6839939c3413e337a53
Brandon Chinn <brandonchinn178@gmail.com>
2022-12-27 19:51:46 -0800

    Generate with Jinja instead of mustache

--- scripts/generate/run.sh
+++ scripts/generate/run.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
+set -eux -o pipefail
+
 cd "$(dirname "${BASH_SOURCE[0]}")"
-exec stack GeneratePersistentAPI.hs
+
+if [[ ! -d venv ]]; then
+    python3 -m venv venv
+fi
+
+venv/bin/pip install Jinja2 PyYAML
+venv/bin/python generate.py
